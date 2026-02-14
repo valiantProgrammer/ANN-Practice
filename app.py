e1eabccf9af487505cd860003693cf7928e15525
valiantProgrammer <rupayandey134@gmail.com>
2026-02-14 20:38:41 +0530

    1.0.0.4

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
+import pandas as pd
+import pickle
